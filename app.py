bfb8db31a6ef9b0aa3691614bb2bbf0e84b948d8
oo33 <hasnaouiyacine59@gmail.com>
2026-04-15 16:37:59 +0100

    fix connection leak using context managers

--- app.py
+++ app.py
@@ -11,27 +11,18 @@ def get_db():
     return psycopg2.connect(os.getenv('DATABASE_URL'))
 
 def init_db():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS visits (ip VARCHAR(45) UNIQUE)')
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_db() as conn:
+        with conn.cursor() as cur:
+            cur.execute('CREATE TABLE IF NOT EXISTS visits (ip VARCHAR(45) UNIQUE)')
 
 @app.route('/api/v1/<ip>')
 def status(ip):
-    
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute('SELECT 1 FROM visits WHERE ip = %s', (ip,))
-    if cur.fetchone():
-        cur.close()
-        conn.close()
-        return jsonify({'message': 'exist'}), 200
-    cur.execute('INSERT INTO visits (ip) VALUES (%s)', (ip,))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_db() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT 1 FROM visits WHERE ip = %s', (ip,))
+            if cur.fetchone():
+                return jsonify({'message': 'exist'}), 200
+            cur.execute('INSERT INTO visits (ip) VALUES (%s)', (ip,))
     return jsonify({'message': 'ok'}), 200
 
 if __name__ == '__main__':
